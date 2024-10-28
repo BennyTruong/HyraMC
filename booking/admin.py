c75c377ccaaa7d4b6f47fdd5ad34806bebc39a40
BennyTruong <benny@hotmail.se>
2024-10-28 16:59:18 +0100

    Added contactmessage to the django admin interface

--- booking/admin.py
+++ booking/admin.py
@@ -1,9 +1,16 @@
 # bookings/admin.py
 
 from django.contrib import admin
-from .models import Booking, Motorcycle, Service
+from .models import Booking, Motorcycle, Service, ContactMessage
 
 # Register your models here
 admin.site.register(Booking)
 admin.site.register(Motorcycle)
 admin.site.register(Service)
+
+# Register the ContactMessage model to appear in the admin interface
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'created_at')
+    search_fields = ('name', 'email', 'subject')
+    list_filter = ('created_at',)
